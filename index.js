a15f20f0b2225b4fe08fd0283d4881418bf3c4a4
Mark Silva <mark.allen.silva@gmail.com>
2022-09-03 13:43:20 -0700

    starting js logic

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 
+const clear = document.querySelector(".clear")
+
+const dateElement = document. getElementById("date")
+const list = document.getElementById("list")
+const input = doucment.getElementById("input")
+
