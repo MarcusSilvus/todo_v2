33b2a1fdd43c18e6bd456de42cd885f02d3397aa
Mark Silva <mark.allen.silva@gmail.com>
2022-09-04 20:19:52 -0700

    created toDo function and keyListener

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 
+list = []
 
 const clear = document.querySelector(".clear")
+const enterKey = document.querySelector("plus-btn")
 
 const dateElement = document. getElementById("date")
 const list = document.getElementById("list")
 const input = doucment.getElementById("input")
+
+function addToDo(toDo) {
+  const text = `<li class="item">
+                  <i class="fa fa-circle-thin" job ="complete"></i>
+                  <p class="text"> ${toDo} </p>
+                  <i class="fa fa-trash-o" job="delete"></i>
+                </li>`
+
+  const position = "beforeend"
+
+  list.insertAdjacentElement(position, text)
+} 
+
+enterKey.addEventListener("keyup", (e) => {
+  if(e.keyCode === 13) {
+    const toDo = input.value;
+    if(toDo) {
+      addToDo(toDo)
+    }
+    input.value = ""
+  }  
+})
